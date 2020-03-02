621fc9862484cad1d2ae642f6b7a1fd5f746170c
eda3 <gerorin1010@gmail.com>
2020-03-03 03:39:23 +0900

    first commit

---
+++ 01_init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+
+
+sudo apt-get install build-essential curl file git
+
+sudo apt install -y xclip
+sudo apt install -y ansible
+
+
+
---
+++ 02_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -xu
+
+git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+mkdir ~/.linuxbrew/bin
+ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+eval $(~/.linuxbrew/bin/brew shellenv)
